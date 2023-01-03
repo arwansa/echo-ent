690f93924242ba901a6811ac5f610ed931c742c9
Arwan Salafudin Ashari <arwansalafudina@gmail.com>
2023-01-03 08:22:35 +0700

    simple open db connection

--- config/ent.go
+++ config/ent.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"time"
 
 	"github.com/arwansa/echo-ent/ent"
 	_ "github.com/go-sql-driver/mysql"
@@ -28,12 +27,7 @@ func NewEntClient() (*ent.Client, error) {
 		cfg.DB.User, cfg.DB.Pass, cfg.DB.Host, cfg.DB.Port, cfg.DB.Name,
 	)
 
-	client, err := ent.Open("mysql", dsn, ent.Log(func(i ...interface{}) {
-		for _, v := range i {
-			fmt.Println(time.Now().Format("2006-01-02 15:04:05"), v)
-			fmt.Print("\n")
-		}
-	}))
+	client, err := ent.Open("mysql", dsn)
 
 	if err != nil {
 		log.Fatalf("failed opening connection to mysql: %v", err)
